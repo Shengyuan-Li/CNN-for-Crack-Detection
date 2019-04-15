68407a5eb27af029bb7f2aa84e5283af6d02ea21
Shengyuan-Li <40750672+Shengyuan-Li@users.noreply.github.com>
2019-04-15 11:41:04 +0800

    Update slide.m

--- demo/slide.m
+++ demo/slide.m
@@ -25,20 +25,20 @@ for i =1:step_r:(size(image,1)-rows+1)
     end
 end
 
-% % 2-nd sliding
-% for i =step_r/2:step_r:(size(image,1)-rows+1)
-%     for j=step_c/2:step_c:(size(image,2)-cols+1)
-%         im=image(i:i+rows-1, j:j+cols-1,:);
-%         imshow(im);
-%         [~, maxlabel] = caffe_classification_part2(im ,net, model_dir); 
-%         switch maxlabel
-%         case 1
-%              m=m+1;
-% %              imwrite(im,['D:\caffe-master\matlab\demo\0\',num2str(m),'.jpg']);
-%              out_image(i:i+rows-1,j:j+cols-1,:)=im;
-%         case 2
-%              n=n+1;
-% %              imwrite(im,['D:\caffe-master\matlab\demo\1\',num2str(n),'.jpg']);
-%         end
-%     end
-% end
+% 2-nd sliding
+for i =step_r/2:step_r:(size(image,1)-rows+1)
+    for j=step_c/2:step_c:(size(image,2)-cols+1)
+        im=image(i:i+rows-1, j:j+cols-1,:);
+        imshow(im);
+        [~, maxlabel] = caffe_classification_part2(im ,net, model_dir); 
+        switch maxlabel
+        case 1
+             m=m+1;
+%              imwrite(im,['D:\caffe-master\matlab\demo\0\',num2str(m),'.jpg']);
+             out_image(i:i+rows-1,j:j+cols-1,:)=im;
+        case 2
+             n=n+1;
+%              imwrite(im,['D:\caffe-master\matlab\demo\1\',num2str(n),'.jpg']);
+        end
+    end
+end
